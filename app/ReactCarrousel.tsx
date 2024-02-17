952e138250b6a77c3fff101dff6b2c886eb1da35
Josué Castillo <josuecastillo@bisual.net>
2024-02-17 22:20:24 +0100

    Rendiment optimization

--- app/ReactCarrousel.tsx
+++ app/ReactCarrousel.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel CSS
 import Image from 'next/image';
+
 interface ImageObject {
   src: string;
   alt: string;
@@ -11,21 +12,17 @@ interface CarouselProps {
   images: ImageObject[];
 }
 
-const disableScroll = () => {
-  document.body.style.overflow = 'hidden';
-};
-
-const enableScroll = () => {
-  document.body.style.overflow = '';
-};
-
 const DemoCarousel: React.FC<CarouselProps> = ({ images }) => {
-  const handleSwipeStart = () => {
-    disableScroll();
-  };
+  const [loadedImages, setLoadedImages] = useState([images[0].src]); // Initially load only the first image
 
-  const handleSwipeEnd = () => {
-    enableScroll();
+  const handleLoadImage = (index: number) => {
+    // Load image if not already loaded
+    if (!loadedImages.includes(images[index].src)) {
+      setLoadedImages((currentLoadedImages) => [
+        ...currentLoadedImages,
+        images[index].src,
+      ]);
+    }
   };
 
   return (
@@ -36,18 +33,27 @@ const DemoCarousel: React.FC<CarouselProps> = ({ images }) => {
         showThumbs={false}
         showArrows={true}
         showStatus={false}
-        swipeable={false}
-        emulateTouch={false}
+        swipeable={true}
+        emulateTouch={true}
         useKeyboardArrows={true}
         dynamicHeight={false}
         swipeScrollTolerance={20}
-        // Uncomment these if you have defined the event handlers
-        // onSwipeStart={handleSwipeStart}
-        // onSwipeEnd={handleSwipeEnd}
+        onChange={(index) => handleLoadImage(index)} // Assuming `onChange` provides the new active index
       >
         {images.map((image, index) => (
           <div key={index}>
-            <Image src={image.src} width={560} height={620} alt={image.alt} />
+            {loadedImages.includes(image.src) ? (
+              <Image src={image.src} width={560} height={620} alt={image.alt} />
+            ) : (
+              // Optionally render a placeholder or nothing until the image is loaded
+              <div
+                style={{
+                  width: '560px',
+                  height: '620px',
+                  backgroundColor: '#f0f0f0',
+                }}
+              ></div>
+            )}
             <p
               style={{
                 position: 'absolute',
@@ -58,7 +64,6 @@ const DemoCarousel: React.FC<CarouselProps> = ({ images }) => {
                 fontSize: '25px',
                 fontFamily: 'Playfair Display',
                 fontOpticalSizing: 'auto',
-                // fontWeight: <weight>;
                 fontWeight: 700,
               }}
             >
