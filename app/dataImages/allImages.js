5e85a8dafe160cd117b2abe09ca3fa5909adf9b6
Josué Castillo <josuecastillo@bisual.net>
2025-08-24 14:10:05 +0200

    Update alt text for images in allImages.js; improve descriptions for better context in the memory carousel

--- app/dataImages/allImages.js
+++ app/dataImages/allImages.js
@@ -10,23 +10,23 @@ const carouselImages = [
     { src: '/memorias/dormirSuelo/dormirSuelo_2.png', alt: 'Durmiendo en el suelo' },
   ],
   [
-    { src: '/memorias/barcelona/barcelona_1.png', alt: ''  },
+    { src: '/memorias/barcelona/barcelona_1.png', alt: 'Cita romantica en Barcelona'  },
   ],
   [
-    { src: '/memorias/zooDeCastellar/zooDeCastellar_1.jpeg', alt: '' },
+    { src: '/memorias/zooDeCastellar/zooDeCastellar_1.jpeg', alt: 'Tour por españa hasta Castellar' },
     { src: '/memorias/zooDeCastellar/zooDeCastellar_2.jpeg', alt: '' },
     { src: '/memorias/zooDeCastellar/zooDeCastellar_3.jpeg', alt: '' },
   ],
   [
-    { src: '/memorias/cabarceno/cabarceno_1.jpeg', alt: '' },
-    { src: '/memorias/cabarceno/cabarceno_2.jpeg', alt: '' },
+    { src: '/memorias/cabarceno/cabarceno_1.jpeg', alt: 'Visitando Cabarceno' },
+    { src: '/memorias/cabarceno/cabarceno_2.jpeg', alt: 'Visitando Cabarceno' },
   ],
   [
-    { src: '/memorias/vallDeNuria/vallDeNuria_1.png', alt: '' },
+    { src: '/memorias/vallDeNuria/vallDeNuria_1.png', alt: 'Primer viaje con la familia de la Nadine' },
   ],
   [
-    { src: '/memorias/coraxFauna/coraxFauna_1.jpeg', alt: '' },
-    { src: '/memorias/coraxFauna/coraxFauna_2.jpeg', alt: '' },
+    { src: '/memorias/coraxFauna/coraxFauna_1.jpeg', alt: 'Carax Fauna, un lobo casi me come, pero fui increible' },
+    { src: '/memorias/coraxFauna/coraxFauna_2.jpeg', alt: 'Carax Fauna, un lobo casi me come, pero fui increible' },
   ]
 ];
 
